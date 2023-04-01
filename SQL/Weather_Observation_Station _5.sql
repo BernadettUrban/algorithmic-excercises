cc99149386eda69944b0291fdb0cca9216dca453
Bernadett Urban <bernadett.urban@outlook.com>
2023-04-01 13:52:40 +0200

    Solution for Weather Observation Station 5

---
+++ SQL/Weather_Observation_Station _5.sql
@@ -0,0 +1,6 @@
+/*
+ https://www.hackerrank.com/challenges/weather-observation-station-5/problem?isFullScreen=false
+ */
+(SELECT CITY, LENGTH(CITY)FROM STATION ORDER BY LENGTH(CITY) ASC, CITY ASC LIMIT 1)
+UNION
+(SELECT CITY, LENGTH(CITY) FROM STATION ORDER BY LENGTH(CITY) DESC, CITY ASC LIMIT 1);
