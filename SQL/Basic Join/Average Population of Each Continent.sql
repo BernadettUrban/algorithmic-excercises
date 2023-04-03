3229a194dd1f39e910d8313555455f15ac29b2a7
Bernadett Urbán <bernadett.urban@outlook.com>
2023-04-03 07:29:45 +0200

    Average Population of Each Continent

---
+++ SQL/Basic Join/Average Population of Each Continent.sql
@@ -0,0 +1,6 @@
+/*
+https://www.hackerrank.com/challenges/average-population-of-each-continent/problem?isFullScreen=true
+*/
+SELECT COUNTRY.Continent, FLOOR(AVG(CITY.Population)) FROM CITY
+INNER JOIN COUNTRY ON CITY.CountryCode = COUNTRY.Code
+GROUP BY COUNTRY.Continent
