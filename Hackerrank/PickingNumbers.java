f31d29c885df0e5259a4467ff32387c947d6b5fa
Bernadett Urbán <bernadett.urban@outlook.com>
2023-04-18 13:53:52 +0200

    Update PickingNumbers.java

--- Hackerrank/PickingNumbers.java
+++ Hackerrank/PickingNumbers.java
@@ -4,49 +4,15 @@ public class PickingNumbers {
 
     public static void main(String[] args) {
 
-        List<Integer> a = new ArrayList(Arrays.asList(
-                // 1, 1, 2, 2, 4, 4, 5, 5, 5));
-                1, 2, 2, 3, 1, 2));
+        List<Integer> a = new ArrayList(Arrays.asList(1, 2, 2, 3, 1, 2));
         System.out.println(pickingNumbers(a));
         Collections.sort(a);
-        // System.out.println(a);
+        
     }
 
     public static int pickingNumbers(List<Integer> a) {
         int max = 0;
-        /*
-         * Collections.sort(a);
-         * 
-         * List<List<Integer>> listOfList = new ArrayList<>();
-         * List<Integer> list = new ArrayList<>();
-         * // list.add(a.get(0));
-         * for (int i = 0; i < a.size(); i++) {
-         * for (int j = 0; j < a.size(); j++) {
-         * if (Math.abs(a.get(j) - a.get(i)) <= 1) {
-         * 
-         * list.add(a.get(j));
-         * // list.add(a.get(i + 1));
-         * // listOfList.add(list);
-         * // System.out.println(a.get(i + 1));
-         * // System.out.println(listOfList);
-         * System.out.println(list);
-         * 
-         * }
-         * listOfList.add(list);
-         * list.removeAll(list);
-         * 
-         * }
-         * 
-         * }
-         * 
-         * System.out.println(listOfList);
-         * for (int i = 0; i < listOfList.size(); i++) {
-         * if (listOfList.get(i).size() > max) {
-         * max = listOfList.get(i).size();
-         * }
-         * }
-         */
-
+       
         List<Integer> subarray = new ArrayList<Integer>();
 
         for (int i = 0; i < a.size(); i++) {
